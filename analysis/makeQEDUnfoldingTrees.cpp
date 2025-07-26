446131a4e3104476a236d4a81e5ad7a421dcd8e9
Pranati Jana <tukupranati@gmail.com>
2025-07-26 20:07:43 +0200

    Phi modulation added

--- analysis/makeQEDUnfoldingTrees.cpp
+++ analysis/makeQEDUnfoldingTrees.cpp
@@ -23,18 +23,23 @@ const double eleMass = 0.5109989461e-3;
 Double_t getDR  ( Double_t eta1, Double_t phi1, Double_t eta2, Double_t phi2);
 Double_t getDPHI( Double_t phi1, Double_t phi2);
 Double_t getDETA(Double_t eta1, Double_t eta2);
+double cosphotonpair(TLorentzVector p1, TLorentzVector pair, bool helicityFrame );
+double costhetastar_CS(TLorentzVector p1, TLorentzVector pair); 
+float  GetAngle( float vPX1, float vPY1, float vPX2, float vPY2);
 
 float gen_Pt1;
 float gen_Eta1;
 float gen_Phi1;
+float gen_PID1;
 float gen_Pt2;
 float gen_Eta2;
 float gen_Phi2;
+float gen_PID2;
 float gen_diEle_M;
 float gen_diEle_Pt;
 float gen_diEle_Rapidity;
-
-
+float gen_diEle_Costhetastar;
+float gen_diEle_PhiModulation;
 int run;
 int ls;
 int evtnb;
@@ -63,6 +68,7 @@ float vSum_ee_Pt;
 float vSum_ee_Eta;
 float vSum_ee_Phi;
 float vSum_ee_Rapidity;
+float ele_PhiModulation;
 float ele_dpt;
 float ele_deta;
 float ele_dphi;
@@ -73,21 +79,32 @@ int ok_chexcl;
 int ok_chexcl_extrk;
 int nExtrk;
 int ok_trigger;
+int ok_event;
 
+float zdc_energy_pos;
+float zdc_energy_neg;
+
+float costhetastar;
+float cos_photon_pair_helicity0;
+float cos_photon_pair_helicity1;
 
 /// initialise gen tree
 void InitGenTree(TTree *genTree) {
   genTree->Branch("gen_Pt1",  &gen_Pt1, "gen_Pt1/F");
   genTree->Branch("gen_Eta1", &gen_Eta1, "gen_Eta1/F");
   genTree->Branch("gen_Phi1", &gen_Phi1, "gen_Phi1/F");
+  genTree->Branch("gen_PID1", &gen_PID1, "gen_PID1/F");
 
   genTree->Branch("gen_Pt2",  &gen_Pt2, "gen_Pt2/F");
   genTree->Branch("gen_Eta2", &gen_Eta2, "gen_Eta2/F");
   genTree->Branch("gen_Phi2", &gen_Phi2, "gen_Phi2/F");
+  genTree->Branch("gen_PID2", &gen_PID2, "gen_PID2/F");
 
   genTree->Branch("gen_diEle_M",  &gen_diEle_M,  "gen_diEle_M/F");
   genTree->Branch("gen_diEle_Pt", &gen_diEle_Pt, "gen_diEle_Pt/F");
   genTree->Branch("gen_diEle_Rapidity", &gen_diEle_Rapidity, "gen_diEle_Rapidity/F");
+  genTree->Branch("gen_diEle_Costhetastar", &gen_diEle_Costhetastar, "gen_diEle_Costhetastar/F");
+  genTree->Branch("gen_diEle_PhiModulation", &gen_diEle_PhiModulation, "gen_diEle_PhiModulation/F");
 }
 
 
@@ -97,14 +114,18 @@ void InitTree(TTree *tr) {
   tr->Branch("gen_Pt1",  &gen_Pt1, "gen_Pt1/F");
   tr->Branch("gen_Eta1", &gen_Eta1, "gen_Eta1/F");
   tr->Branch("gen_Phi1", &gen_Phi1, "gen_Phi1/F");
-  
+  tr->Branch("gen_PID1", &gen_PID1, "gen_PID1/F");
+
   tr->Branch("gen_Pt2",  &gen_Pt2, "gen_Pt2/F");
   tr->Branch("gen_Eta2", &gen_Eta2, "gen_Eta2/F");
   tr->Branch("gen_Phi2", &gen_Phi2, "gen_Phi2/F");
+  tr->Branch("gen_PID2", &gen_PID2, "gen_PID2/F");
   
   tr->Branch("gen_diEle_M",  &gen_diEle_M,  "gen_diEle_M/F");
   tr->Branch("gen_diEle_Pt", &gen_diEle_Pt, "gen_diEle_Pt/F");
   tr->Branch("gen_diEle_Rapidity", &gen_diEle_Rapidity, "gen_diEle_Rapidity/F");
+  tr->Branch("gen_diEle_Costhetastar", &gen_diEle_Costhetastar, "gen_diEle_Costhetastar/F");
+  tr->Branch("gen_diEle_PhiModulation", &gen_diEle_PhiModulation, "gen_diEle_PhiModulation/F");
   
   tr->Branch("run",                 &run,           "run/I");
   tr->Branch("ls",                  &ls,            "ls/I");
@@ -134,6 +155,7 @@ void InitTree(TTree *tr) {
   tr->Branch("vSum_Eta",            &vSum_ee_Eta,     "vSum_ee_Eta/F");
   tr->Branch("vSum_Phi",            &vSum_ee_Phi,     "vSum_ee_Phi/F");
   tr->Branch("vSum_Rapidity",       &vSum_ee_Rapidity,"vSum_ee_Rapidity/F");
+  tr->Branch("ele_PhiModulation",   &ele_PhiModulation,"ele_PhiModulation/F");
   tr->Branch("ele_dpt",             &ele_dpt,         "ele_dpt/F");
   tr->Branch("ele_deta",            &ele_deta,        "ele_deta/F");
   tr->Branch("ele_dphi",            &ele_dphi,        "ele_dphi/F");
@@ -144,6 +166,14 @@ void InitTree(TTree *tr) {
   tr->Branch("ok_chexcl_extrk",     &ok_chexcl_extrk, "ok_chexcl_extrk/I");
   tr->Branch("nExtrk",              &nExtrk,          "nExtrk/I");
   tr->Branch("ok_trigger",          &ok_trigger,      "ok_trigger/I");
+  tr->Branch("ok_event",            &ok_event,      "ok_event/I");
+  tr->Branch("zdc_energy_pos",             &zdc_energy_pos,         "zdc_energy_pos/F");
+  tr->Branch("zdc_energy_neg",             &zdc_energy_neg,         "zdc_energy_neg/F");
+
+  tr->Branch("costhetastar",        &costhetastar,        "costhetastar/F");
+  tr->Branch("cos_photon_pair_helicity0",     &cos_photon_pair_helicity0,     "cos_photon_pair_helicity0/F");
+  tr->Branch("cos_photon_pair_helicity1",     &cos_photon_pair_helicity1,     "cos_photon_pair_helicity1/F");
+  //
 }
 
 // reset gen variables
@@ -151,12 +181,16 @@ void ResetGenVars() {
   gen_Pt1 = 0;
   gen_Eta1 = 0;
   gen_Phi1 = 0;
+  gen_PID1 = 0;
   gen_Pt2 = 0;
   gen_Eta2 = 0;
   gen_Phi2 = 0;
+  gen_PID2 = 0;
   gen_diEle_M = 0;
   gen_diEle_Pt = 0;
   gen_diEle_Rapidity = 0;
+  gen_diEle_Costhetastar = -999;
+  gen_diEle_PhiModulation = -999;
 }
 
 
@@ -190,6 +224,7 @@ void ResetVars() {
   vSum_ee_Eta = 0 ;
   vSum_ee_Phi = 0 ;
   vSum_ee_Rapidity = 0 ;
+  ele_PhiModulation = 0;
   ele_dpt = 0;
   ele_deta = 0;
   ele_dphi = 0;
@@ -199,7 +234,14 @@ void ResetVars() {
   ok_chexcl = 0;
   ok_chexcl_extrk = 0;
   nExtrk = 0;
-  ok_trigger = 0;
+  ok_trigger = 1;
+  ok_event = 0;
+  zdc_energy_pos = 0;
+  zdc_energy_neg = 0;
+  
+  costhetastar = -999;
+  cos_photon_pair_helicity0 = -999;
+  cos_photon_pair_helicity1 = -999;
 }
 
 
@@ -239,13 +281,14 @@ int main(int argc, char* argv[])
   
   auto events = make_unique<EventProcessor>(inputPath, dataset);
   
-  int trigger_passed=0, twoGoodEle=0, oppCharge=0, neutral_excl=0, charged_excl=0, dielemass_wozdc=0, dielept_wozdc=0;
+  int event_passed=0, trigger_passed=0, twoGoodEle=0, oppCharge=0, neutral_excl=0, charged_excl=0, dielemass_wozdc=0, dielept_wozdc=0;
   int acop_cut_wozdc=0, dielemass=0, dielept=0, acop_cut = 0, zdc_excl=0;
   
   // Loop over events
   for(int iEvent=0; iEvent<events->GetNevents(); iEvent++){
-  //for(int iEvent=0; iEvent<10000; iEvent++){
-    if(iEvent%1000 == 0) Log(0)<<"Processing event "<<iEvent<<"\n";
+//  for(int iEvent=0; iEvent<100; iEvent++){
+//if(iEvent > 200) continue;
+    if(iEvent%10000 == 0) Log(0)<<"Processing event "<<iEvent<<"\n";
     //if(iEvent >= config.params("maxEvents")) break;
     
     auto event = events->GetEvent(iEvent);
@@ -253,44 +296,74 @@ int main(int argc, char* argv[])
     ResetGenVars();  
     ResetVars();  
     
-    if(sampleName == "QED_SC"  || sampleName == "QED_SL" ){   
-      auto genP1 = event->GetPhysObjects(EPhysObjType::kGenParticle)[0]; 
-      auto genP2 = event->GetPhysObjects(EPhysObjType::kGenParticle)[1];
+   /* if(sampleName == "QED_SL"){   
+      // cout << "PID1:" <<  genP1->GetPID() << "  two:" << genP2->GetPID() << "\n";  
+      auto genP1 = event->GetPhysObjects(EPhysObjType::kGenParticle)[0]; //3 for SC
+      auto genP2 = event->GetPhysObjects(EPhysObjType::kGenParticle)[1]; // 4 for SC
+
+      //cout << "PID1:" <<  genP1->GetPID() << "  two:" << genP2->GetPID() << "\n";     
+      */ 
+  if(sampleName == "QED_SC" ){
+	auto genP1 = event->GetPhysObjects(EPhysObjType::kGenParticle)[3]; //3 for SC // 0 for SL
+	auto genP2 = event->GetPhysObjects(EPhysObjType::kGenParticle)[4]; // 4 for SC // 1 for SL
       
-      //if(sampleName == "QED_SC" ){
-//	auto genP1 = event->GetPhysObjects(EPhysObjType::kGenParticle)[2]; 
-//	auto genP2 = event->GetPhysObjects(EPhysObjType::kGenParticle)[3];
- //     }
       
       ResetGenVars();
       if(abs(genP1->GetPID())== 11 && abs(genP2->GetPID())== 11 ){
-	//Log(0) << " PID 1:" << genP1->GetPID() << "  two:" << genP2->GetPID() << "\n";
+	//Log(0) << " PID one:" << genP1->GetPID() << "  two:" << genP2->GetPID() << "\n";
 	gen_Pt1  = genP1->GetEt();
 	gen_Eta1 = genP1->GetEta();
 	gen_Phi1 = genP1->GetPhi();
-	
+	gen_PID1 = genP1->GetPID();
 	gen_Pt2  = genP2->GetEt();
 	gen_Eta2 = genP2->GetEta();
 	gen_Phi2 = genP2->GetPhi();
+  gen_PID2 = genP2->GetPID();
+      //  cout << "Tesrt:" << genP2->GetEt() << endl;	
+	TLorentzVector gen_ele1, gen_ele2, gen_diele, gen_ele;
+	gen_ele1.SetPtEtaPhiM(gen_Pt1, gen_Eta1, gen_Phi1, 0.000511);
+	gen_ele2.SetPtEtaPhiM(gen_Pt2, gen_Eta2, gen_Phi2, 0.000511);
 	
-	TLorentzVector ele1, ele2, diele;
-	ele1.SetPtEtaPhiM(gen_Pt1, gen_Eta1, gen_Phi1, 0.000511);
-	ele2.SetPtEtaPhiM(gen_Pt2, gen_Eta2, gen_Phi2, 0.000511);
-	
-	diele = ele1 + ele2;
-	gen_diEle_M   = diele.M();
-	gen_diEle_Pt  = diele.Pt();
-	gen_diEle_Rapidity = diele.Rapidity();
+	gen_diele = gen_ele1 + gen_ele2;
+
+	gen_diEle_M   = gen_diele.M();
+	gen_diEle_Pt  = gen_diele.Pt();
+	gen_diEle_Rapidity = gen_diele.Rapidity();
+	gen_diEle_Costhetastar = costhetastar_CS(gen_ele1,gen_diele);
+
+  //momentum components to determine the angle between the two vectors
+
+    if(gen_PID1 == 11){
+    gen_ele.SetPtEtaPhiM(gen_Pt1, gen_Eta1, gen_Phi1, 0.000511);
+    //cout << "neg charge:" << gen_PID1<< ":gen pt1:" << gen_Pt1 << endl;
+   }
+   else if (gen_PID1 == -11){
+    gen_ele.SetPtEtaPhiM(gen_Pt2, gen_Eta2, gen_Phi2, 0.000511);
+    //cout << "neg charge:" << gen_PID2 <<  ":gen pt2:" << gen_Pt2 << endl;
+   }
+        float GenDPx = gen_diele.Pt()*cos(gen_diele.Phi());
+        float GenDPy = gen_diele.Pt()*sin(gen_diele.Phi());
+        float GenMPx = gen_ele.Pt()*cos(gen_ele.Phi());
+        float GenMPy = gen_ele.Pt()*sin(gen_ele.Phi());
+///////////////
+	//gen_diEle_PhiModulation = GetAngle( GenDPx, GenDPy, GenMPx, GenMPy );  //Gen delta phi between 0 and pi// for SC
+  gen_diEle_PhiModulation = GetAngle( GenDPx, GenDPy, GenMPx, GenMPy ); // For SL
+  //cout << " Gen phi modulation:" << gen_diEle_PhiModulation << endl;
+
 	
       } // PID
-
+}
         // Check trigger
+      event_passed++;
+      hist->SetBinContent(1,event_passed);          
       ok_trigger = (event->HasTrigger(kDoubleEG2noHF));
       
+      //trigger_passed++;
+          
+    //  if(!event->HasTrigger(kDoubleEG2noHF)) continue;
       trigger_passed++;
-      hist->SetBinContent(1,trigger_passed);
+      hist->SetBinContent(2,trigger_passed);
       hist_wozdc->SetBinContent(1,trigger_passed);
-      
       run = event->GetRunNumber();
       ls = event->GetLumiSection();
       evtnb = event->GetEventNumber();
@@ -298,22 +371,28 @@ int main(int argc, char* argv[])
       // select two exclusive electrons for QED event selection
       if(event->GetPhysObjects(EPhysObjType::kGoodElectron).size() == 2){
 	twoGoodEle++;
-	hist->SetBinContent(2,twoGoodEle);     hist_wozdc->SetBinContent(2,twoGoodEle);
+	hist->SetBinContent(3,twoGoodEle);     hist_wozdc->SetBinContent(2,twoGoodEle);
 	
 	auto genTracks = event->GetPhysObjects(EPhysObjType::kGoodGeneralTrack);
 	auto electron1 = event->GetPhysObjects(EPhysObjType::kGoodElectron)[0];
 	auto electron2 = event->GetPhysObjects(EPhysObjType::kGoodElectron)[1];
 	auto caloTower = event->GetPhysObjects(EPhysObjType::kCaloTower);
-	
+  auto muons = event->GetPhysObjects(EPhysObjType::kGoodMuon); 
+
+	//if(electron1->GetCharge() == electron2->GetCharge()) continue;
 	if(electron1->GetCharge() != electron2->GetCharge()) {
 	oppCharge++;
-	hist->SetBinContent(3,oppCharge);      hist_wozdc->SetBinContent(3,oppCharge);
+	hist->SetBinContent(4,oppCharge);      hist_wozdc->SetBinContent(3,oppCharge);
 	
 	// event variables
 	ok_neuexcl = (!event->HasAdditionalTowers());
 	ok_chexcl  = (genTracks.size()==2);
-	if(sampleName == "Data")ok_zdcexcl = event->GetTotalZDCenergyPos() < 10000 && event->GetTotalZDCenergyNeg() < 10000;
-	
+        
+	if(sampleName == "Data"){
+        ok_zdcexcl = event->GetTotalZDCenergyPos() < 10000 && event->GetTotalZDCenergyNeg() < 10000;
+	zdc_energy_pos = event->GetTotalZDCenergyPos();
+        zdc_energy_neg = event->GetTotalZDCenergyNeg();
+        }
 	// start filling extra track information here ........................................
 	int nextratracks =0;
     for (auto trk : genTracks) {
@@ -323,9 +402,8 @@ int main(int argc, char* argv[])
     }
 
         if(nextratracks>0)Log(0) << "Event :" << iEvent << " extra tracks:" << nextratracks << "\n";
-	
 	nExtrk = nextratracks ;
-	ok_chexcl_extrk = (nExtrk==0);
+	ok_chexcl_extrk = (nExtrk==0 && muons.size()==0);
 	
 	eleCharge_1  = electron1->GetCharge();
 	elePt_1      = electron1->GetPt();
@@ -335,7 +413,7 @@ int main(int argc, char* argv[])
 	eleSCEta_1   = electron1->GetEtaSC();
 	eleSCPhi_1   = electron1->GetPhiSC();
 	eleEoverP_1  = electron1->GetEoverP();
-	
+//	cout << "charge:" << eleCharge_1 << ":" << "Pt:" << elePt_1 << endl;
 	eleCharge_2  = electron2->GetCharge();
 	elePt_2      = electron2->GetPt();
 	eleEta_2     = electron2->GetEta();
@@ -345,7 +423,7 @@ int main(int argc, char* argv[])
 	eleSCPhi_2   = electron2->GetPhiSC();
 	eleEoverP_2  = electron2->GetEoverP(); 
 	
-	TLorentzVector ele1, ele2, diele;
+	TLorentzVector ele1, ele2, diele, ele;
 	ele1.SetPtEtaPhiM(elePt_1,eleEta_1,elePhi_1,eleMass);
 	ele2.SetPtEtaPhiM(elePt_2,eleEta_2,elePhi_2,eleMass);
 	
@@ -361,9 +439,33 @@ int main(int argc, char* argv[])
 	ele_deta = fabs(eleEta_1 - eleEta_2);
 	ele_dphi = getDPHI(elePhi_1,elePhi_2);
 	ele_acop = 1- (ele_dphi/3.141592653589);  
+
+   cos_photon_pair_helicity0 = cosphotonpair(ele1, diele, 0); // Boost of one photon in the pair direction (in the rest frame of the pair). The other will be at pi rads from the 1st.
+   cos_photon_pair_helicity1 = cosphotonpair(ele1, diele, 1); 
+   costhetastar = costhetastar_CS(ele1,diele);
+ 
+   if(eleCharge_1 == -1){
+    ele.SetPtEtaPhiM(elePt_1,eleEta_1,elePhi_1,eleMass);
+   // cout << "neg charge:" << eleCharge_1 << ":" << "neg Pt:" << elePt_1 << endl;
+   }
+   else if (eleCharge_1 == 1){
+    ele.SetPtEtaPhiM(elePt_2,eleEta_2,elePhi_2,eleMass);
+   // cout << "neg charge:" << eleCharge_2 << ":" << "neg Pt:" << elePt_2 << endl;
+   }
+    //creating the 4vectors for Reconstrutcted MC
+
+                float RecoDPx = diele.Pt()*cos(diele.Phi());
+                float RecoDPy = diele.Pt()*sin(diele.Phi());
+                float RecoMPx = ele.Pt()*cos(ele.Phi());
+                float RecoMPy = ele.Pt()*sin(ele.Phi());
+
+///////////////////////////////////////
+		ele_PhiModulation = GetAngle( RecoDPx, RecoDPy, RecoMPx, RecoMPy ); // Reco deltaphi between -pi and pi
+   // cout << "Reco phi modulation:" << ele_PhiModulation << endl;
+
 	} //electron charge
-      } //if two good electrons
-    } //samplename
+   } //if two good electrons
+     //samplename
     
     tr->Fill(); 
   } //nevents
@@ -520,3 +622,59 @@ double SF_uncert_trig(double et, double eta) {
       else return 0.12731;
    }
 }
+
+
+
+
+double cosphotonpair(TLorentzVector p1, TLorentzVector pair, bool helicityFrame )
+{
+
+  double cos = 0.;
+
+  // Boost of one photon in the pair direction (i.e. to the rest frame of the pair). The other will be at pi rads from the 1st.
+  //   //TVector3 vpair = pair.Vect();
+  TVector3 vpair = pair.BoostVector();
+  p1.Boost(-vpair);
+
+  cos = TMath::Cos( p1.Angle( pair.Vect() ));
+
+  if (helicityFrame) return cos;
+
+  TVector3 zaxis = TVector3(0.,0.,1.);
+  cos = TMath::Cos( p1.Angle( zaxis ));
+
+  return cos;
+
+}
+float GetAngle( float vPX1, float vPY1, float vPX2, float vPY2)
+{
+
+   TVector2 DimuonVect;
+   TVector2 MuonVect;
+
+   DimuonVect.Set( vPX1, vPY1 );
+   MuonVect.Set( vPX2, vPY2 );
+
+   float DimuonMag = sqrt( DimuonVect.X()*DimuonVect.X() + DimuonVect.Y()*DimuonVect.Y()   );
+   float MuonMag = sqrt( MuonVect.X()*MuonVect.X() + MuonVect.Y()*MuonVect.Y()   );
+
+
+   float cosAngle = ( DimuonVect.X()*MuonVect.X() + DimuonVect.Y()*MuonVect.Y() )/( DimuonMag*MuonMag );
+   float sinAngle = ( DimuonVect.X()*MuonVect.Y() - DimuonVect.Y()*MuonVect.X() )/( DimuonMag*MuonMag );
+   float tanAngle = atan2(sinAngle, cosAngle);
+
+   return tanAngle;
+
+}
+
+
+double costhetastar_CS(TLorentzVector p1, TLorentzVector pair)
+{
+
+double costhetastarCS = 2.*(pair.E()*p1.Pz()-pair.Pz()*p1.E())/(pair.M()*pair.Mt());
+
+  return costhetastarCS;
+
+}
+
+
